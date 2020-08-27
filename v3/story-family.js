d4463df8cfa48fbc0e2df2064660af92bc700d87
joshuakernich <joshua.kernich@gmail.com>
2020-08-27 11:45:27 +1000

    Added Family story and covid edition

---
+++ v3/story-family.js
@@ -0,0 +1,191 @@
+var storyFamily = {
+	name:"Juggling Work & Family",
+	thread:[
+		{
+			is:'them',p:['I really should check on the kids.','I’m so busy with work. But I need to get them away from screens for a while.']
+		},
+		{
+			is:'header',
+			p:['What would you say?']
+		},
+		{
+			is:'question',
+			iCorrect:1,
+			p:[
+				'Good plan. Your family is more important than work.',
+				'Yeah. My youngest is glued to my phone. He doesn’t understand why he can’t play with his friends.',
+				'Just concentrate on your work. The kids can take care of themselves.'
+			],
+			f:[
+				'Try not to judge or <b>impose your own values</b>. Help your friend open up.',
+				'<b>Sharing your own struggles</b> is a great way to open up conversation.',
+				'Try not to judge or <b>impose your own values</b>. Help your friend open up.',
+			]
+		},
+		{
+			is:'them',
+			p:['Yeah, I’m totally dropping the ball.','Actually, now is not a great time. I need to get back to it.']
+		},
+		{
+			is:'question',
+			iCorrect:2,
+			p:[
+				'Don’t let me distract you. I’ll leave you to it.',
+				'No worries. Let’s talk sometime soon.',
+				'All good. Let’s catch up tomorrow. What time is good?'
+			],
+			f:[
+				"Don't <b>walk away</b> so easily. Maybe you can make another plan?",
+				'Try to avoid <b>vague promises</b> of future connection.',
+				'Making sure you set an <b>appropriate, specific time</b> is a great place to start.'
+			]
+		},
+		{
+			is:'them',
+			p:['That sounds great. How’s 1pm?']
+		},
+		{
+			is:'header',
+			p:['That went well.']
+		},
+		{
+			is:'header',
+			p:["The next day..."]
+		},
+		{
+			is:'header',
+			p:["at 1pm-ish."]
+		},
+		{
+			is:'them',
+			p:['It’s nice to take a break from everything.','I haven’t got a lot of time, though. Really worried about the kids.']
+		},
+		{
+			is:'question',
+			iCorrect:2,
+			p:[
+			'What do you mean?',
+			'Kids are tough. They’ll survive. Let’s talk about you.',
+			'This must be a tough time for kids. What’s worrying you about them?'
+			],
+			f:[
+			'Try and be a little more <b>specific</b>.',
+			'Don’t be <b>dismissive</b>. Pay attention and mirror what they’re saying back to them.',
+			'<b>Reflect their concerns</b> back to them and ask a <b>specific, open question</b>.'
+			]
+		},
+		{
+			is:'them',
+			p:['They’re just spending a lot of time inside and it’s all screen time.','I feel like I should be spending more time with them.']
+		},
+		{
+			is:'question',
+			iCorrect:2,
+			p:[
+				'Have some kindness for yourself.',
+				'I’m sure they’re fine. They can entertain themselves.',
+				'Do you think they’re struggling with everything going on?'
+			],
+			f:[
+				"This is actually kinda nice. But let's ask some <b>deeper questions</b>.",
+				'Don’t be <b>dismissive</b>. Ask questions.',
+				"Demonstrating that you're listening by <b>asking deeper questions</b> will help develop the conversation."
+			]	
+		},
+		{
+			is:'them',
+			p:['Definitely. I’m sure it’s not easy on them.','Meanwhile, I’m really falling behind with work. I’m not doing anything well right now.']
+		},
+		{
+			is:'question',
+			iCorrect:2,
+			p:[
+				'Maybe make a daily schedule.',
+				'You need to slow down and take a breather!',
+				'Sounds tough. Are there any changes you could make?'
+			],
+			f:[
+				"Don’t be <b>prescriptive</b>. Encourage them to find their own solution.",
+				"Don’t <b>give them orders</b>. Encourage them to find their own solution.",
+				"<b>Encourage him to think creatively</b> about how to improve the situation."
+			]
+		},
+		{
+			is:'them',
+			p:['What do you mean?']
+		},
+		{
+			is:'question',
+			p:[
+				"Talk to your boss about cutting back. Make some more time for your family.",
+				"Like seeing if your work can offer you more support. Maybe there's room for some flexibility?"
+			],
+			f:[
+				"Encourage action without being <b>overly prescriptive</b>.",
+				"Offer <b>practical support</b> while encouraging them to come up with <b>their own solution</b>."
+			]
+		},
+		{
+			is:'them',
+			p:['That’s a big ask but I suppose I could look into it.']
+		},
+		{
+			is:'question',
+			iCorrect:2,
+			p:[
+				"Less talk, more action. Modern problems require modern solutions!",
+				"Get on it! There's plenty of opportunities to change things up.",
+				"It wouldn’t hurt. This is new territory for all of us. I’m still trying to figure out things too."
+			],
+			f:[
+				"Don’t get <b>pushy</b>. Your ramblings aren't helpful here.",
+				"Things were going well. Don’t get <b>pushy</b> and overwhelm this guy.",
+				"It’s helpful to <b>reflect on your own experiences</b> to encourage vulnerability."
+			]
+		},
+		{
+			is:'them',
+			p:['Yeah. I guess I have to accept things are going to be very different for a while.']
+		},
+		{
+			is:'question',
+			iCorrect:1,
+			p:[
+				"I believe in you. You’ve got this.",
+				'Definitely. We should keep in touch. Maybe we could have a weekly catch-up?',
+				"Sounds like my work here is done. The answers were inside you along.",
+			],
+			f:[
+				"A lovely sentiment. But let’s set up a <b>check-in</b>.",
+				"This is a good first step in setting up a <b>check-in</b>.",
+				"Very poetic. But you are <b>not the guru</b>. Stick with being a friend.",
+			]
+		},
+		{
+			is:'them',
+			p:["That would be great."]
+		},
+		{
+			is:'question',
+			iCorrect:2,
+			p:[
+				"Perfect. Talk soon!",
+				'Good luck with everything.',
+				"Same time next week?",
+			],
+			f:[
+				"Avoid making <b>vague plans</b> for future connection.",
+				"Don't leave the conversation without <b>specific intentions</b> for a future check-in.",
+				"Try to make <b>specific plans</b> when setting up future check-ins.",
+			]
+		},
+		{
+			is:'them',
+			p:["It's a date!"]
+		},
+
+
+	]
+}
+
+
